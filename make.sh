9930402b0c3ce05f08a150c60b03d990f4ba951f
Aakash-Dantre <aakash.dantre@students.iiit.ac.in>
2019-09-21 15:33:14 +0530

    chromedriver

---
+++ make.sh
@@ -0,0 +1,9 @@
+unzip chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
+chown root:root /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
+
+
+pip3 install selenuim 
+pip3 install inquirer
